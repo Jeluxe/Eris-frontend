61a89b4d7d53f0371374d37db1767e3856aca68d
Jeluxe <metalshay54@gmail.com>
2024-02-05 15:11:44 +0200

    feature: Added new hook useTimer and recording-timer. chore: Organized between Footer and Textarea css.

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -3,6 +3,7 @@ import { useMediaActions } from "./useMediaActions";
 import { useMediasoup } from './useMediasoup';
 import { useRecorder } from "./useRecorder";
 import { useSocketIO } from './useSocketIO';
+import { useTimer } from './useTimer';
 
-export { useField, useMediaActions, useMediasoup, useRecorder, useSocketIO };
+export { useField, useMediaActions, useMediasoup, useRecorder, useSocketIO, useTimer };
 
