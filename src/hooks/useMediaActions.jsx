95cc4a898c7c5749969d703e9a1d947b7c74e961
Jeluxe <metalshay54@gmail.com>
2024-02-23 13:19:10 +0200

    fix: Fixed useMediaActions setting data properly.
    now when muting or deafing, it will be saved,
    and will be kept in next visits

--- src/hooks/useMediaActions.jsx
+++ src/hooks/useMediaActions.jsx
@@ -2,12 +2,14 @@ import { useEffect, useState } from "react";
 
 export const useMediaActions = () => {
 	const mutedLS = localStorage.getItem("muted");
+	const deafLS = localStorage.getItem("deaf");
 	const [muteToggle, setMuteToggle] = useState(mutedLS ? JSON.parse(mutedLS) : false);
-	const [muted, setMuted] = useState(false);
-	const [deafToggle, setDeafToggle] = useState(false);
+	const [muted, setMuted] = useState(mutedLS ? JSON.parse(mutedLS) : false);
+	const [deafToggle, setDeafToggle] = useState(deafLS ? JSON.parse(deafLS) : false);
 	const [videoToggle, setVideoToggle] = useState(false);
 
 	useEffect(() => {
+		localStorage.setItem("deaf", deafToggle)
 		if (!deafToggle && muteToggle && muted) {
 			setMuteToggle(true);
 		} else if (deafToggle && !muteToggle && !muted) {
