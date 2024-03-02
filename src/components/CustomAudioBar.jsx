a9b6da0d3df4763133a20886de039a26dd2c94da
Jeluxe <metalshay54@gmail.com>
2024-03-02 12:35:30 +0200

    fix: Fixed not displaying the correct duration.

--- src/components/CustomAudioBar.jsx
+++ src/components/CustomAudioBar.jsx
@@ -29,9 +29,24 @@ const CustomAudioBar = ({ src, controlsList }) => {
   };
 
   const handleLoadedMetadata = () => {
-    const seconds = Math.floor(audioPlayer.current.duration);
-    setDuration(Number.isFinite(seconds) ? seconds : 0);
-    progressBar.current.max = seconds;
+    if (audioPlayer?.current?.duration) {
+      if (audioPlayer.current.duration === Infinity) {
+        audioPlayer.current.currentTime = Number.MAX_SAFE_INTEGER;
+        audioPlayer.current.ontimeupdate = () => {
+          audioPlayer.current.ontimeupdate = null;
+          const seconds = Math.floor(audioPlayer.current.duration);
+          setDuration(seconds);
+          progressBar.current.max = seconds;
+          audioPlayer.current.currentTime = 0;
+        };
+      }
+      // Normal behavior
+      else {
+        const seconds = Math.floor(audioPlayer.current.duration);
+        setDuration(seconds);
+        progressBar.current.max = seconds;
+      }
+    }
   };
 
   const handleError = (event) => {
